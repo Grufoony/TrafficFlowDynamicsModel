fec1a53b950cc4bf9f5c505525f3c48dbbdddd47
Grufoony <64806874+Grufoony@users.noreply.github.com>
2022-06-21 21:11:32 +0000

    Deploying to main from  @ 2d068542d161afcc940b066ffde6746f97912c4d 🚀

--- generator.py
+++
@@ -1,8 +0,0 @@
-import networkx as nx
-import numpy as np
-
-G = nx.grid_2d_graph(10,12)
-mat = nx.adjacency_matrix(G)
-with open('matrix.dat','wb') as f:
-    for line in mat.todense():
-        np.savetxt(f, line, fmt='%.2f')
