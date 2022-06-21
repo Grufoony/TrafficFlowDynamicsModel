5eb5661fc2f17f32b042f547989c86d4d925474d
Grufoony <gregorio_2000@hotmail.it>
2022-06-21 17:58:04 +0200

    bin fixes

--- macro.cpp
+++ macro.cpp
@@ -3,9 +3,9 @@
 void macro() {
     auto canv = new TCanvas("canv", "canv", 4096, 2160);
     canv->Divide(2,2);
-    auto h1 = new TH1F("h1", "titolo", 100, 1, 100);
-    auto h2 = new TH1F("h2", "titolo", 100, 1, 100);
-    auto h3 = new TH1F("h3", "titolo", 100, 1, 100);
+    auto h1 = new TH1F("h1", "titolo", 25, 1, 100);
+    auto h2 = new TH1F("h2", "titolo", 25, 1, 100);
+    auto h3 = new TH1F("h3", "titolo", 25, 1, 100);
     std::ifstream fIn;
     double data;
     fIn.open("./temp_data/3000_t.dat");
@@ -13,12 +13,13 @@ void macro() {
         h1->Fill(data);
     }
     fIn.close();
+    h1->Scale(1./h1->Integral(), "nosw2");
     fIn.open("./temp_data/5200_t.dat");
     while(fIn >> data) {
         h2->Fill(data);
     }
     fIn.close();
-    fIn.open("./temp_data/11900_t.dat");
+    fIn.open("./temp_data/10900_t.dat");
     while(fIn >> data) {
         h3->Fill(data);
     }
@@ -26,13 +27,13 @@ void macro() {
 
     canv->cd(1);
     h1->Fit("gaus");
-    h1->Draw("HIST,SAME");
+    h1->Draw("hist, same");
     canv->cd(2);
     // auto f2 = new TF1("f2","[0]*TMath::Exp([1]*x)", 30, 100);
-    // auto f2 = new TF1("f2","[0]*TMath::Gaus(TMath::Log(x), [1], [2])/x", 0, 100);
-    // f2->SetParameters(4.5, 30, 100);
-    // h2->Fit(f2, "R");
-    h2->Fit("gaus");
+    auto f2 = new TF1("f2","[0]*TMath::Gaus(TMath::Log(x), [1], [2])/x", 0, 100);
+    f2->SetParameters(4.5, 30, 100);
+    h2->Fit(f2, "R");
+    // h2->Fit("gaus");
     h2->Draw("HIST,SAME");
     canv->cd(3);
     // auto g1= new TF1("g1","gaus", 0, 100);
