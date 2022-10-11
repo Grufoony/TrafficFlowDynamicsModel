2a5c924f52127bc57a35a365879f1d10f12c904d
SimonB00 <simone.balducci@gmail.com>
2022-10-11 21:11:14 +0200

    Implemented installation with pip

--- setup.py
+++ setup.py
@@ -2,17 +2,21 @@ from glob import glob
 from setuptools import setup, find_packages
 from pybind11.setup_helpers import Pybind11Extension, build_ext
 
+__version__ = '1.0.0'
+
 ext_modules = [
 	Pybind11Extension(
 		"TrafficModel",
-		sorted(glob("source/*.cpp"))
+        ["TrafficModel/binding.cc"]
 	),
 ]
 
 setup(
 	name="TrafficModel",
+    version=__version__,
 	author="Gregorio Berselli, Simone Balducci",
+    description="",
 	packages=find_packages(),
-	cmdclass={"build_ext": build_ext},
+	# cmdclass={"build_ext": build_ext},
 	ext_modules=ext_modules
 )
