c0879af2bb593937b8c9191b95de0057d48ba76c
Grufoony <gregorio_2000@hotmail.it>
2022-11-14 23:05:39 +0100

    security update

--- TrafficModel/SparseMatrix.hpp
+++ TrafficModel/SparseMatrix.hpp
@@ -14,8 +14,10 @@ template <typename T> class SparseMatrix {
 public:
   SparseMatrix() = default;
   SparseMatrix(int rows, int cols) {
-    this->_rows = rows;
-    this->_cols = cols;
+    rows < 0 || cols < 0
+        ? throw std::invalid_argument("SparseMatrix: rows and cols must be > 0")
+        : _rows = rows,
+          _cols = cols;
   };
   SparseMatrix(SparseMatrix const &other) {
     this->_rows = other._rows;
@@ -24,30 +26,37 @@ public:
   };
 
   void insert(int i, int j, T value) {
-    if (i >= _rows || j >= _cols) {
+    if (i >= _rows || j >= _cols || i < 0 || j < 0) {
       throw std::out_of_range("Index out of range");
     }
     _matrix.emplace(std::make_pair(i * _cols + j, value));
   };
   void insert(int i, T value) {
-    if (i >= _rows * _cols) {
+    if (i >= _rows * _cols || i < 0) {
       throw std::out_of_range("Index out of range");
     }
     _matrix.emplace(std::make_pair(i, value));
   };
   void insert_or_assign(int i, int j, T value) {
-    if (i >= _rows || j >= _cols) {
+    if (i >= _rows || j >= _cols || i < 0 || j < 0) {
       throw std::out_of_range("Index out of range");
     }
     _matrix.insert_or_assign(i * _cols + j, value);
   };
-  void erase(int i, int j) { _matrix.erase(i * _cols + j); };
+  void erase(int i, int j) {
+    _matrix.count(i * _cols + j)
+        ? _matrix.erase(i * _cols + j)
+        : throw std::out_of_range("Index out of range");
+  };
   void clear() noexcept { _matrix.clear(); };
   bool exists(int i, int j) const noexcept {
     return _matrix.count(i * _cols + j);
   };
 
   std::unordered_map<int, T> getRow(int index) const {
+    if (index >= _rows || index < 0) {
+      throw std::out_of_range("Index out of range");
+    }
     std::unordered_map<int, T> row;
     for (auto &it : _matrix) {
       if (it.first / _cols == index) {
@@ -57,6 +66,9 @@ public:
     return row;
   }
   std::unordered_map<int, T> getCol(int index) const {
+    if (index >= _cols || index < 0) {
+      throw std::out_of_range("Index out of range");
+    }
     std::unordered_map<int, T> col;
     for (auto &it : _matrix) {
       if (it.first % _cols == index) {
@@ -93,7 +105,7 @@ public:
   }
 
   T const &operator()(int i, int j) {
-    if (i >= _rows || j >= _cols) {
+    if (i >= _rows || j >= _cols || i < 0 || j < 0) {
       throw std::out_of_range("Index out of range");
     }
     if (_matrix.count(i * _cols + j)) {
