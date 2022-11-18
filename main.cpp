8a0bc24f7850d283a0bca4a8bb88061a1ba33a18
Grufoony <gregorio_2000@hotmail.it>
2022-11-18 09:12:23 +0100

    small changes

--- main.cpp
+++ main.cpp
@@ -59,7 +59,8 @@ int main(int argc, char **argv) {
 
   auto g = Graph(argv[1]);
   // std::ofstream fOut;
-  int dVehicle;
+  int dVehicle = 0;
+  int totTime = 0;
 
   switch (argc) {
   case 2:
@@ -87,17 +88,18 @@ int main(int argc, char **argv) {
     std::cout << "Setting up the simulation..." << '\n';
     Vehicle::addVehicleType(argv[2]);
     g.setTemperature(std::stod(argv[3]));
+    g.fprint(true);
     g.updateTransMatrix();
     dVehicle = std::stoi(argv[4]);
     std::cout << "Done." << '\n';
-    g.fprint(true);
     clearDir(DATA_FOLDER);
     clearDir(IMG_FOLDER);
     // TEST
     // printTransMatrices();
     // adiabatic analysis
-    for (int t = 0; t < std::stoi(argv[5]); ++t) {
-      printLoadingBar(t, std::stoi(argv[5]));
+    totTime = std::stoi(argv[5]);
+    for (int t = 0; t < totTime; ++t) {
+      printLoadingBar(t, totTime);
       if (t % 100 == 0) {
         // g.fprintHistogram(DATA_FOLDER, "density", 10, "latex");
         // g.fprintDistribution(DATA_FOLDER, "u/q");
@@ -105,43 +107,37 @@ int main(int argc, char **argv) {
         // g.fprintDistribution(DATA_FOLDER, "u/k");
         // g.fprintActualState(DATA_FOLDER, "q/k");
         // g.fprintActualState(DATA_FOLDER, "u/k");
-        // g.fprintTimeDistribution(DATA_FOLDER, "k", 0.);
+        g.fprintTimeDistribution(DATA_FOLDER, "k", 0.);
         // g.fprintTimeDistribution(DATA_FOLDER, "q", 0.);
         // g.fprintTimeDistribution(DATA_FOLDER, "u", 0.);
-        g.fprintHistogram(DATA_FOLDER, "traveltime", 20, "latex");
-        g.fprintHistogram(DATA_FOLDER, "traveltime", 20, "root");
+        // g.fprintHistogram(DATA_FOLDER, "traveltime", 20, "latex");
+        // g.fprintHistogram(DATA_FOLDER, "traveltime", 20, "root");
       }
       // if (t == 3000 || t == 5200 || t == 11400) {
       //   g.fprintVisual("./temp_img/data/");
       // }
       // constant analysis
-      if (t < 12e3 && t % 60 == 0 && t != 0) {
-        g.addVehiclesUniformly(dVehicle / 1.125);
-        g.addRndmVehicles(dVehicle);
-      }
-      // peaked analysis
-      // if (t < 500 && t % 50 == 0 && t != 0) {
-      //   g.addVehiclesUniformly(dVehicle / 10);
+      // if (t < 12e3 && t % 60 == 0 && t != 0) {
+      //   // g.addVehiclesUniformly(dVehicle / 1.125);
+      //   g.addRndmVehicles(dVehicle);
+      //   // g.addVehiclesUniformly(dVehicle);
       // }
-      // if (t == 0) {
-      //   g.fprintVisual(IMG_FOLDER);
-      // }
-      // if (t % 240 == 0 && t != 0) {
-      //   g.test();
-      // }
-      // constant analysis
       // if (t < 8e3) {
       //   g.evolve();
       // } else {
       //   g.evolve(false);
       // }
+
       // peaked analysis
-      // if (t < 0.4e4) {
-      //   g.evolve();
-      // } else {
-      //   g.evolve(false);
-      // }
-      g.evolve(false);
+      if (t < 500 && t % 50 == 0 && t != 0) {
+        g.addVehiclesUniformly(dVehicle / 10);
+      }
+      if (t < 0.4e4) {
+        g.evolve();
+      } else {
+        g.evolve(false);
+      }
+      // g.evolve(false);
     }
     break;
 
@@ -157,8 +153,9 @@ int main(int argc, char **argv) {
     g.fprint(true);
     clearDir(DATA_FOLDER);
     clearDir(IMG_FOLDER);
-    for (int t = 0; t < std::stoi(argv[6]); ++t) {
-      printLoadingBar(t, std::stoi(argv[6]));
+    totTime = std::stoi(argv[6]);
+    for (int t = 0; t < totTime; ++t) {
+      printLoadingBar(t, totTime);
       if ((t % 300 == 0) && (t < (std::stoi(argv[5]) * 3 / 2))) {
         dVehicle = std::abs(std::stoi(argv[4]) *
                             std::sin(2 * M_PI * t / std::stoi(argv[5])));
