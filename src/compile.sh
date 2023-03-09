a2f5df7da8ae5b653ae19d28478bcf7efa0e38ce
Grufoony <64806874+Grufoony@users.noreply.github.com>
2023-03-09 23:31:36 +0100

    added move line

--- src/compile.sh
+++ src/compile.sh
@@ -1,3 +1,4 @@
 #
 
 g++ -O3 -Wall -shared -std=c++20 -fPIC $(python3 -m pybind11 --includes) ./binding.cpp -o TrafficModel$(python3-config --extension-suffix)
+mv *.so ~/.local/lib/python*/site-packages
