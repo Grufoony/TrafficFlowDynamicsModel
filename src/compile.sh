27bfac5e05a5bc0e1246a485d5c93a366643a420
SimonB00 <simone.balducci@gmail.com>
2023-03-09 22:00:08 +0100

    Corrected path to binding.cpp in compile.sh

--- src/compile.sh
+++ src/compile.sh
@@ -1,3 +1,3 @@
 #
 
-g++ -O3 -Wall -shared -std=c++20 -fPIC $(python3 -m pybind11 --includes) ./src/binding.cpp -o TrafficModel$(python3-config --extension-suffix)
+g++ -O3 -Wall -shared -std=c++20 -fPIC $(python3 -m pybind11 --includes) ./binding.cpp -o TrafficModel$(python3-config --extension-suffix)
