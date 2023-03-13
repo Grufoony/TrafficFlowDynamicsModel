8754f9552333e40274e12c5ab1fc6d94bc80dcf1
Grufoony <gregorio_2000@hotmail.it>
2023-03-13 10:28:31 +0100

    removed SparseMatrix

--- src/binding.cpp
+++ src/binding.cpp
@@ -80,37 +80,4 @@ PYBIND11_MODULE(TrafficModel, m) {
       .def("incrementTimeTraveled", &Vehicle::incrementTimeTraveled)
       .def("getTimeTraveled", &Vehicle::getTimeTraveled)
       .def("resetTimeTraveled", &Vehicle::resetTimeTraveled);
-
-//   pybind11::class_<SparseMatrix<bool>>(m, "SparseMatrix")
-//       .def(pybind11::init<>())
-//       .def(pybind11::init<int, int>())
-//       .def(pybind11::init<int>())
-//       .def(pybind11::init<SparseMatrix<bool> const &>())
-//       .def("insert", static_cast<void (SparseMatrix<bool>::*)(int, int, bool)>(
-//                          &SparseMatrix<bool>::insert))
-//       .def("insert", static_cast<void (SparseMatrix<bool>::*)(int, bool)>(
-//                          &SparseMatrix<bool>::insert))
-//       .def("insert_or_assign",
-//            static_cast<void (SparseMatrix<bool>::*)(int, int, bool)>(
-//                &SparseMatrix<bool>::insert_or_assign))
-//       .def("insert_or_assign",
-//            static_cast<void (SparseMatrix<bool>::*)(int, bool)>(
-//                &SparseMatrix<bool>::insert_or_assign))
-//       .def("erase", &SparseMatrix<bool>::erase)
-//       .def("clear", &SparseMatrix<bool>::clear)
-//       .def("contains",
-//            static_cast<bool (SparseMatrix<bool>::*)(int, int) const>(
-//                &SparseMatrix<bool>::contains))
-//       .def("contains", static_cast<bool (SparseMatrix<bool>::*)(int) const>(
-//                            &SparseMatrix<bool>::contains))
-//       .def("getRow", &SparseMatrix<bool>::getRow)
-//       .def("getCol", &SparseMatrix<bool>::getCol)
-//       .def("getRndRowElement", &SparseMatrix<bool>::getRndRowElement)
-//       .def("getRndColElement", &SparseMatrix<bool>::getRndColElement)
-//       .def("getNormRows", &SparseMatrix<bool>::getNormRows)
-//       .def("getNormCols", &SparseMatrix<bool>::getNormCols)
-//       .def("getRowDim", &SparseMatrix<bool>::getRowDim)
-//       .def("getColDim", &SparseMatrix<bool>::getColDim)
-//       .def("size", &SparseMatrix<bool>::size)
-//       .def("print", &SparseMatrix<bool>::print);
 }
